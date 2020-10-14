d2e6905ebf00ab06467287b24f5033a3a2e393f5
Nicolas Sarubbi <nicolas@sarubbi.com.ar>
2020-10-14 20:19:36 -0300

    models

--- source/bubble/admin.py
+++ source/bubble/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from bubble import models
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Profile, ProfileAdmin)
--- source/bubble/models.py
+++ source/bubble/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    phone = models.CharField(max_length=20, blank=True)
+    birthdate = models.DateTimeField(null=True, blank=True)
+    user = models.OneToOneField('auth.User', related_name='profile', on_delete=models.CASCADE)
+
+    @property
+    def age(self):
+        return 10  # FIXME
